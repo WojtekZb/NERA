c241ff2041e3571c63bc80a4f2bfa514d6844b04
WojtekZb <554101@student.fontys.nl>
2025-09-22 14:56:50 +0200

    Started work on Event creation

--- NERA/NERA Logic/Entities/Event.cs
+++ NERA/NERA Logic/Entities/Event.cs
@@ -2,12 +2,11 @@
 {
     public class Event
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public DateTime Date { get; set; }
         public string Location { get; set; } = string.Empty;
         public double Cost { get; set; }
-        public int maxPeople { get; set; }
+        public int MaxPeople { get; set; }
         public string Description { get; set; } = string.Empty;
     }
 }
--- NERA/NERA Logic/Services/CreateEventService.cs
+++ NERA/NERA Logic/Services/CreateEventService.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NERA_Logic.Entities;
 using NERA_Logic.Interfaces;
 
 namespace NERA_Logic.Services
 {
     public class CreateEventService : ICreateEvent
-    {
-
+    {   
     }
 }
