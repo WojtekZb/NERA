6e12d69758512eb684ac951720193484fe416938
WojtekZb <554101@student.fontys.nl>
2025-09-23 13:18:17 +0200

    Event creation to be tested. DTO also has errormessages. Fixed minor issues.

--- NERA/NERA Logic/Services/CreateEventService.cs
+++ NERA/NERA Logic/Services/CreateEventService.cs
@@ -6,10 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 using NERA_Logic.Entities;
 using NERA_Logic.Interfaces;
+using NERA_Logic.DTOs;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NERA_Logic.Services
 {
-    public class CreateEventService : ICreateEvent
-    {   
+    public class CreateEventService : ICreateEventService
+    {
+        public Event CreateEvent(EventDTO eventdto)
+        {
+            Event evenement = new Event
+            {
+                Name = eventdto.Name,
+                Date = eventdto.Date,
+                Description = eventdto.Description,
+                Cost = eventdto.Cost,
+                MaxPeople = eventdto.MaxPeople,
+                Location = eventdto.Location
+            };
+            return evenement;
+
+        }
     }
 }
