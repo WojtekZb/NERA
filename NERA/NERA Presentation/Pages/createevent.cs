140aa809b98dda85f7f1139f7ff6c4c8a47f33f6
WojtekZb <554101@student.fontys.nl>
2025-10-07 10:24:02 +0200

    Event creation might be done. Testing needed

--- NERA/NERA Presentation/Pages/createevent.cs
+++ NERA/NERA Presentation/Pages/createevent.cs
@@ -1,12 +1,29 @@
+using Domain.Entities;
+using Logic.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace NERA_Presentation.Pages
 {
+    // Presentation/Pages/Events/Create.cshtml.cs
     public class Index1Model : PageModel
     {
-        public void OnGet()
+        private readonly CreateEventService _service;
+
+        public Index1Model(CreateEventService service)
         {
+            _service = service;
+        }
+
+        [BindProperty]
+        public Event EventDto { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var createdEvent = await _service.CreateEventAsync(EventDto);
+            return RedirectToPage("Details", new { id = createdEvent.Id });
         }
     }
 }
