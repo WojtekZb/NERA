fa2894eba047fc848ef6ecc349e6c921ee49ea7b
fleskens99 <franciscopdelf@gmail.com>
2025-10-08 15:31:23 +0200

    still in process, just problems with the packages, need too change computer

---
+++ NERA/Domain/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+
+namespace YourProjectName.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly YourDbContext _context;
+
+        public EventsController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEvents()
+        {
+            var events = await _context.Events.ToListAsync();
+            return Ok(events);
+        }
+    }
+}
+
