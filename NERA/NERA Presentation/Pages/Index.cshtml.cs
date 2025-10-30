fad52e15c4ac9501a25038bf06a15348b034b114
Diego Barreira <barreirafds>
2025-10-30 10:20:17 +0100

    UPDATED (SAVE)

--- NERA/NERA Presentation/Pages/Index.cshtml.cs
+++ NERA/NERA Presentation/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Data;
 namespace NERA_Presentation.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly AppDbContext _context;
+        public IndexModel(AppDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-
-        public void OnGet()
+        public IList<Event> Events { get; private set; } = new List<Event>();
+        public async Task OnGetAsync()
         {
+            Events = await _context.Events
+                .AsNoTracking()
+                .OrderBy(e => e.StartDate)
+                .ToListAsync();
 
         }
     }
