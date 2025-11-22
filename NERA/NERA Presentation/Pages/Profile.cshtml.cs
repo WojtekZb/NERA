cd5dd74d062c8d7c541630c9d6b35a71c03671d7
Diego Barreira <barreirafds>
2025-11-22 16:55:24 +0100

    PROFILE UI CREATED (FIXED GMAIL) (WE NEED TO USE NICKNAME + @GMAIL TO WORK/SHOW CORRECT)

--- NERA/NERA Presentation/Pages/Profile.cshtml.cs
+++ NERA/NERA Presentation/Pages/Profile.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace NERA_Presentation.Pages
 {
+    [Authorize]
     public class ProfileModel : PageModel
     {
         public void OnGet()
